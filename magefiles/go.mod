0820e1d2f4e1228cf850a1eac79827e68ba8ce18
Anders K. Pettersen <staticaland@users.noreply.github.com>
2023-09-28 00:49:35 +0200

    chore: Add Dagger pipelines to Magefile

--- magefiles/go.mod
+++ magefiles/go.mod
@@ -2,4 +2,16 @@ module github.com/staticaland/brandish/magefiles
 
 go 1.21.1
 
-require github.com/magefile/mage v1.15.0 // indirect
+require (
+	dagger.io/dagger v0.8.7 // indirect
+	github.com/99designs/gqlgen v0.17.31 // indirect
+	github.com/Khan/genqlient v0.6.0 // indirect
+	github.com/adrg/xdg v0.4.0 // indirect
+	github.com/iancoleman/strcase v0.3.0 // indirect
+	github.com/magefile/mage v1.15.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/staticaland/brandish v0.0.0-20230927223346-b29a1f8f6771 // indirect
+	github.com/vektah/gqlparser/v2 v2.5.6 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -3,7 +3,13 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"os"
+
+	"dagger.io/dagger"
 	"github.com/magefile/mage/sh"
+	"github.com/staticaland/brandish/pipelines"
 )
 
 // Build the project.
@@ -14,4 +20,38 @@ func Build() error {
 // Run the project.
 func Run() error {
 	return sh.Run("go", "run", "main.go")
+}
+
+// Lint
+func Lint(ctx context.Context) error {
+
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	fmt.Println(pipelines.Markdownlint(client))
+
+	return nil
+}
+
+// Lint and fix
+func LintFix(ctx context.Context) error {
+
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	fmt.Println(
+		pipelines.Markdownlint(
+			client,
+			pipelines.WithGlobs("README.md"),
+			pipelines.WithFix(),
+		),
+	)
+
+	return nil
 }
